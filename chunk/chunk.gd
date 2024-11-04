d1967da4db1ef924bfd12452b83d409d304389e3
kiwijuice56 <alfaroeric127@gmail.com>
2024-11-03 23:02:59 -0500

    chunk generation

--- chunk/chunk.gd
+++ chunk/chunk.gd
@@ -1,4 +1,17 @@
 class_name Chunk extends GridMap
 
-func generate() -> void:
-	pass
+@export var chunk_size: int = 8
+@export var noise: NoiseTexture2D
+
+func _ready() -> void:
+	await noise.changed
+	generate(Vector3())
+
+func generate(coordinate: Vector3) -> void:
+	position = coordinate
+	for i in range(chunk_size):
+		for j in range(chunk_size):
+			var uv: Vector2 = Vector2(coordinate.x + i, coordinate.y + j).posmod(512) / 512.0
+			var height: int = int(noise.get_image().get_pixel(min(uv.x * noise.get_width(), noise.get_width() - 1), min(uv.y * noise.get_height(), noise.get_height() - 1)).r*8) 
+			for k in range(height):
+				set_cell_item(Vector3i(i, k, j), 0)
